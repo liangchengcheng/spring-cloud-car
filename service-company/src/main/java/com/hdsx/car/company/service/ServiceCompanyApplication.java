7e06b54b6c4cc2bf8725da93683e245a54b04a41
liangchengcheng <1038127753@qq.com>
2017-12-22 08:25:28 +0800

    update druid
    update druid

--- service-company/src/main/java/com/hdsx/car/company/service/ServiceCompanyApplication.java
+++ service-company/src/main/java/com/hdsx/car/company/service/ServiceCompanyApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableFeignClients(basePackages = {"com.hdsx.car.redis","com.hdsx.car.company"})
 @EnableEurekaClient
 @MapperScan(basePackages = "com.hdsx.car.dao")
+@ServletComponentScan
 public class ServiceCompanyApplication {
 
 	public static void main(String[] args) {
