e879e34b4d23d7580a4e50238ff235337a5eef07
liangchengcheng <1038127753@qq.com>
2017-12-30 13:12:50 +0800

    add two common class
    add two common class

---
+++ service-storm/src/main/java/com/hdsx/car/service/strom/common/ConfKeys.java
@@ -0,0 +1,7 @@
+package com.hdsx.car.service.strom.common;
+
+public class ConfKeys {
+
+    public static final String REDIS_HOST = "redis_host_key";
+    public static final String REDIS_PORT = "redis_port_key";
+}
